5ce9290b6814422ea3deaee06581a667d3d5a262
Nathan D Leathers <nathan.d.leathers@icloud.com>
2022-06-01 13:01:13 -0700

    imported code properly still need to add new classes

--- User.py
+++ User.py
@@ -1,1 +1,50 @@
 # your improved User class goes here
+
+import AppIUserCode
+# imports all the code from AppIUserCode.py file
+
+profile = AppIUserCode.User
+# assigns class user to new variable
+
+dante = profile("dante","dvl99@hotmail.com",6666666666,28)
+# creates new user in this file
+
+dante.jersey_num = 45
+# creats new instance variable (note: cannot add files to other files users)
+# -=-=-=-=-=-=-=-=-=--=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=--=-=-=-
+
+current_post = []
+class Status:
+    def __init__(self,post):
+        self.post = post
+
+    def post_history(self,history)
+        self.history = {}
+
+postNum = 0
+post = input("post your thoughts here!")
+
+if post != "":
+    current_post.append([post,postNum+1])
+    
+
+
+dante.current_post = current_post.append()
+
+
+
+
+
+
+
+
+
+
+
+# print(dante.post)
+
+# ideas: 
+# class of Status update to track post history in an object
+# 
+
+
